92369e23259a542126e0489dccd5c9da80d19ee2
szymbaramichal <szymbaramichal@gmail.com>
2024-10-17 21:10:04 +0200

    fix: changing .gitignore file and adding app project

---
+++ ExpenseTracker.App/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ExpenseTracker.App.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
